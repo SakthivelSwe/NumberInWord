7e8d90c167988ab36cbdf8464e7841ead048030b
sakthiveljoker <77488929+sakthiveljoker@users.noreply.github.com>
2021-08-08 10:35:15 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+//        Write a method called printNumberInWord.
+//                The method has one parameter number w
+//        hich is the whole number. The method needs to
+//        print "ZERO", "ONE", "TWO", ... "NINE", "OTHER" if the int parameter
+//        number is 0, 1, 2, .... 9 or other for any other number including negative
+//        numbers. You can use if-else statement or switch statement whatever is easier
+//        for you.
+
+
+
+       NumberInWord.printNumberInWord(-1);
+    }
+}
